15a6d6ae49ba19a87a5b78c769796f81be25e558
josse <jotay234@gmail.com>
2025-09-17 18:37:26 -0500

    feat(routing): configurar rutas iniciales para navegacion basica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/auth/login/login';
+import { Register } from './pages/auth/register/register';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Home } from './pages/home/home';
+import { Profile } from './pages/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: 'home', pathMatch: 'full'},
+    {path:'home', component: Home},
+    {path:'login', component: Login},
+    {path:'register', component: Register},
+    {path:'dashboard', component: Dashboard},
+    {path:'profile', component: Profile},
+    {path:'home', component: Home}, // Manejo de rutas desconocidas
+];
