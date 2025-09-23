17875ba2a6d3ec259eb73c4d621ffe23c5c7f1c3
josse <jotay234@gmail.com>
2025-09-22 21:58:57 -0500

    Establecer login como ruta principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,11 @@ import { Home } from './pages/home/home';
 import { Profile } from './pages/profile/profile';
 
 export const routes: Routes = [
-    {path:'', redirectTo: 'home', pathMatch: 'full'},
+    {path:'', redirectTo: 'login', pathMatch: 'full'},
     {path:'home', component: Home},
     {path:'login', component: Login},
     {path:'register', component: Register},
     {path:'dashboard', component: Dashboard},
     {path:'profile', component: Profile},
-    {path:'home', component: Home}, // Manejo de rutas desconocidas
+    {path:'**', component: Login}, // Manejo de rutas desconocidas
 ];
