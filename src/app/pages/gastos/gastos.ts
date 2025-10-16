3afa38642a7a9237241ef1256cc3c4be0e3fd87d
josse <jotay234@gmail.com>
2025-10-16 17:14:14 -0500

    feat(ingresos-gastos): se agregaron las vistas de ingresos y gastos

--- src/app/pages/gastos/gastos.ts
+++ src/app/pages/gastos/gastos.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-gastos',
-  imports: [],
+  imports: [RouterModule, Navbar, CommonModule],
   templateUrl: './gastos.html',
   styleUrl: './gastos.css'
 })
 export class Gastos {
+gastos = [
+    { descripcion: 'Almuerzo', monto: 15000, fecha: new Date() },
+    { descripcion: 'Transporte', monto: 8000, fecha: new Date() },
+  ];
 
+  agregarGasto() {
+    console.log('Agregar nuevo gasto');
+    // Aquí abrirías un modal o redirigirías al formulario de agregar gasto
+  }
+
+  editarGasto(gasto: any) {
+    console.log('Editar gasto:', gasto);
+    // Aquí abrirías el formulario con los datos del gasto seleccionado
+  }
+
+  eliminarGasto(gasto: any) {
+    console.log('Eliminar gasto:', gasto);
+    // Aquí podrías mostrar una confirmación antes de eliminarlo
+  }
 }
