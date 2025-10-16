7306b3bd0904b9da3980ec1e48411bbc1df53beb
josse <jotay234@gmail.com>
2025-10-15 22:01:05 -0500

    feat(components):agregaron navbar y dashboard como componentes reutilizables

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.html',
+  imports: [RouterModule],
+  styleUrls: ['./navbar.css'],
+})
+export class Navbar{
+  isMenuOpen = false;
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+}
