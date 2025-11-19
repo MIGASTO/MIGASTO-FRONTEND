b7e591dbcccc3c02729f9616261e43dc91cffbe2
candelo02 <candeloj2002@gmail.com>
2025-11-18 19:13:09 -0500

    fix: corregir navegación en navbar (logout con AuthService, mantener rutas correctas)

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.html',
-  imports: [RouterModule, ],
+  imports: [RouterModule],
   styleUrls: ['./navbar.css'],
 })
-export class Navbar{
+export class Navbar {
   isMenuOpen = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   closeMenu() {
     this.isMenuOpen = false;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+    this.closeMenu();
   }
 }
